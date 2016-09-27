6735980593ec1b3715d22e8eaef41a3c185837a8
arelbernardo <arelbernardo@gmail.com>
2016-09-27 16:16:33 +0800

    Creation of Base Controller and Model

---
+++ application/core/Base_controller.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: arel
+ * Date: 27/09/2016
+ * Time: 4:15 PM
+ */
+class Base_controller extends CI_Controller
+{
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
---
+++ application/core/Base_model.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: arel
+ * Date: 27/09/2016
+ * Time: 4:15 PM
+ */
+class Base_model extends CI_Model
+{
+    public function __construct() {
+        parent::__construct();
+    }
+}
